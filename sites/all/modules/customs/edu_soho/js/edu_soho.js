f831e8382200b75994083751ecca1d7ab52181df
dale <dale.guo@loweprofero.com>
2015-09-01 19:46:12 +0800

    v1

--- sites/all/modules/customs/edu_soho/js/edu_soho.js
+++ sites/all/modules/customs/edu_soho/js/edu_soho.js
@@ -2,7 +2,7 @@
 Drupal.behaviors.webrtc = {
   attach: function(context) {  
   	$('#open_class_record').click(function(){
-  		 order_id = location.search.split('/')[3];
+  		 order_id = location.search.split('/')[4];
   		 uid = Drupal.settings.currentUser;
   		 $.ajax({
                 type: 'POST',
@@ -15,8 +15,12 @@ Drupal.behaviors.webrtc = {
                     	alert('您已经申请过了，我们的老师会尽快与您联系！');	
                     }else if(data.error==2){
                     	alert('您点卡不足，请充值后申请！！');	
+                      $('#open_class_record').slideUp();
+                    }else if(data.error==3){
+                      alert('该课程不允许再申请了！！');  
+                      $('#open_class_record').slideUp();
                     }else if(data.nid){
-                    	alert('创建成功！请耐性等待老师与您联系！');
+                    	alert('创建成功！请耐心等待老师与您联系！');
                     }else{
                     	alert('创建失败了！！');
                     }
@@ -27,6 +31,40 @@ Drupal.behaviors.webrtc = {
                 }
             });
   	});
+  
+    $('.open_class_record').click(function(e){
+       e.preventDefault();
+       order_id = $(this).attr('title');
+       uid = Drupal.settings.currentUser;
+       $.ajax({
+                type: 'POST',
+                url: Drupal.settings.basePath+'?q=create/class_record',
+                data: 'uid=' + uid + '&order_id=' + order_id,
+                dataType:'json',
+                success: function (data) {
+                    //Swap state values
+                    if(data.error==1){
+                      alert('您已经申请过了，我们的老师会尽快与您联系！'); 
+                      $(this).slideUp();
+                    }else if(data.error==2){
+                      alert('您点卡不足，请充值后申请！！'); 
+                      $(this).slideUp(); 
+                    }else if(data.error==3){
+                      alert('该课程不允许再申请了！！');  
+                      $(this).slideUp();
+                    }else if(data.nid){
+                      alert('创建成功！请耐心等待老师与您联系！');
+                      $(this).slideUp();
+                    }else{
+                      alert('创建失败了！！');
+                    }
+                    // $('#open_class_record').slideUp();
+                    // $("#gallonsDonated").html(data.gals);
+                    // $("#familiesServed").html(data.fams);
+                    // $(".info").removeClass("loading");
+                }
+            });
+    });
 
   	}
   }
