f28010c12a0760c6c8295d402c98757eaa3f5ed1
dale <dale.guo@loweprofero.com>
2015-09-15 18:44:43 +0800

    module for theme

--- sites/all/modules/customs/edu_soho/js/edu_soho.js
+++ sites/all/modules/customs/edu_soho/js/edu_soho.js
@@ -9,25 +9,24 @@ Drupal.behaviors.webrtc = {
                 url: Drupal.settings.basePath+'?q=create/class_record',
                 data: 'uid=' + uid + '&order_id=' + order_id,
                 dataType:'json',
+                context: this,
                 success: function (data) {
                     //Swap state values
                     if(data.error==1){
                     	alert('您已经申请过了，我们的老师会尽快与您联系！');	
+                      $(this).hide(); 
                     }else if(data.error==2){
                     	alert('您点卡不足，请充值后申请！！');	
-                      $('#open_class_record').slideUp();
+                      $(this).hide(); 
                     }else if(data.error==3){
                       alert('该课程不允许再申请了！！');  
-                      $('#open_class_record').slideUp();
+                      $(this).hide(); 
                     }else if(data.nid){
                     	alert('创建成功！请耐心等待老师与您联系！');
                     }else{
                     	alert('创建失败了！！');
+                      $(this).hide(); 
                     }
-                    // $('#open_class_record').slideUp();
-                    // $("#gallonsDonated").html(data.gals);
-                    // $("#familiesServed").html(data.fams);
-                    // $(".info").removeClass("loading");
                 }
             });
   	});
@@ -41,27 +40,24 @@ Drupal.behaviors.webrtc = {
                 url: Drupal.settings.basePath+'?q=create/class_record',
                 data: 'uid=' + uid + '&order_id=' + order_id,
                 dataType:'json',
-                success: function (data) {
+                context: this,
+                success: function (data,text) {
                     //Swap state values
                     if(data.error==1){
                       alert('您已经申请过了，我们的老师会尽快与您联系！'); 
-                      $(this).slideUp();
+                      $(this).hide();
                     }else if(data.error==2){
                       alert('您点卡不足，请充值后申请！！'); 
-                      $(this).slideUp(); 
+                      $(this).hide(); 
                     }else if(data.error==3){
                       alert('该课程不允许再申请了！！');  
-                      $(this).slideUp();
+                      $(this).hide();
                     }else if(data.nid){
                       alert('创建成功！请耐心等待老师与您联系！');
-                      $(this).slideUp();
+                      $(this).hide();
                     }else{
                       alert('创建失败了！！');
                     }
-                    // $('#open_class_record').slideUp();
-                    // $("#gallonsDonated").html(data.gals);
-                    // $("#familiesServed").html(data.fams);
-                    // $(".info").removeClass("loading");
                 }
             });
     });
--- sites/all/modules/customs/edu_soho/js/open_classroom.js
+++ sites/all/modules/customs/edu_soho/js/open_classroom.js
@@ -1,7 +1,8 @@
 (function($){
 Drupal.behaviors.openclassroom = {
   attach: function(context) {
-     	$('#open_classroom').click(function(){
+     	$('#open_classroom').click(function(e){
+     		e.preventDefault();
      		window.open($(this).attr('data-room'),"ABC-ChinaEdu","directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=800,height=600");
      	});
   	}
