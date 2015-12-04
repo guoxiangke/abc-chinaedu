5180b4e54e7375ced518d95bd169b33d57f8c682
dale <dale.guo@loweprofero.com>
2015-12-04 18:26:11 +0800

    debug

--- sites/all/themes/thedu/templates/views/calendar/views-view-field--order-calendar--field-class-date-plan.tpl.php
+++ sites/all/themes/thedu/templates/views/calendar/views-view-field--order-calendar--field-class-date-plan.tpl.php
@@ -26,7 +26,9 @@
 $account = $row->field_field_student[0]['raw']['uid'];
 // $account = $row->field_field_teacher[0]['raw']['uid'];
 //从自动创建的节点中获取 理论上课时间：field_class_date_plan
+
 $class_begin = strtotime($row->field_data_field_class_date_plan_field_class_date_plan_value.' UTC+8');//	
+$temp_begin = $class_begin;
 $class_end = strtotime($row->field_data_field_class_date_plan_field_class_date_plan_value_1.' UTC+8');//2015-12-01 07:00:00
 $tip = '';
 // get 那日当天的记录节点！
@@ -36,29 +38,34 @@ if($records){
 	$nid = 0;
 	foreach ($records as $key => $record) {
 		$nid = $key;
-		$class_begin = date('H:i',strtotime($record->extraFields->field_class_time_begin.' UTC+8'));	
+		$temp_begin = strtotime($record->extraFields->field_class_time_begin.' UTC+8');
+		$class_begin = date('H:i',$temp_begin);	
 		$class_end = date('H:i',strtotime($record->extraFields->field_class_time_end.' UTC+8'));
 		break;
 	}
 	$tip .= '已完成： <a href="'.url('node/'.$nid).'">'.$class_begin.'-'.$class_end.'</span></a><br/>';
 }
-
+date_default_timezone_set('Asia/Shanghai');
 $now = time();
-if($now>$class_begin){
+// dpm($now-$class_begin); return;
+if($now>$temp_begin){
 	$tip .= '<span data-pclass="cc-done"></span>';
 	echo $tip;
 	return;
 }else{
-	$tip = '<span data-pclass="cc-coming">计划时间：</span>';
+	$tip .= '<span data-pclass="cc-coming">计划时间：</span>';
 }
-if(date('Y-m-d') == date('Y-m-d',$class_begin)){
+$t = date('Y-m-d');
+$b = date('Y-m-d',$temp_begin);
+// dpm($b);
+if("$t" == "$b"){
 	// get 当天的记录节点！
 	//从上课记录中获取 实际上课时间
-	$tip = ' '.$class_begin.' <span data-pclass="cc-today">今日上课 nid！</span><br/>';
+	$tip = '<a href="'.url('node/'.$nid).'"><span data-pclass="cc-today">今日上课 </span>'.$class_begin.'</a><br/>';
+	echo $tip; return;
+}else{
+	echo $tip; 
 }
-echo $tip;
+
 ?>
 <?php print preg_replace('/\d+\/\d+\/\d+ - /', '', $output); ?>
-
-
-
