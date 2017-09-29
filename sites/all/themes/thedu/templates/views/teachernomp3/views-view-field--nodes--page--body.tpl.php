57b1e7410ec3b91c8b401c66ad6c4aaf0f558c84
daleMAC <dale.guo@BJGLPR-NMX0016.local>
2017-09-29 11:35:25 +0800

    up views tpl for notes&records

--- sites/all/themes/thedu/templates/views/teachernomp3/views-view-field--nodes--page--body.tpl.php
+++ sites/all/themes/thedu/templates/views/teachernomp3/views-view-field--nodes--page--body.tpl.php
@@ -1,6 +1,8 @@
 <?php
 if($output==0){
-  echo l('<span class="glyphicon glyphicon-edit"></span> No!','node/'.$row->nid.'/edit',array('html'=>true,'attributes'=>array('title'=>'Add Class note!')));
+  echo l('<span class="glyphicon glyphicon-edit" style="color:red;"></span>','node/'.$row->nid.'/edit',array('html'=>true,'attributes'=>array('title'=>'Add Class note!')));
+}else{
+  echo l('<span class="glyphicon glyphicon-check"></span>','node/'.$row->nid,array('html'=>true,'attributes'=>array('title'=>'Review Class Notes!')));
 }
 ?>
 <?php //print $output; ?>
--- sites/all/themes/thedu/templates/views/teachernomp3/views-view-field--nodes--page--delta.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-if($output==0){
-  echo l('<span class="glyphicon glyphicon-headphones"></span> No!','node/'.$row->nid.'/edit',array('html'=>true,'attributes'=>array('title'=>'Add Class recording!')));
-}
-?>
-<?php //print $output; ?>
